e1e9cf57938f6d8fb21a772989f3eb892ef57639
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-10-26 21:58:00 +0300

    Changed storage panel view

--- Assets/Scripts/StoragePanel.cs
+++ Assets/Scripts/StoragePanel.cs
@@ -7,7 +7,6 @@ public class StoragePanel : MonoBehaviour
     [SerializeField] private ItemData _itemData;
     [SerializeField] private Image _iconImage;
     [SerializeField] private TextMeshProUGUI _nameText;
-    [SerializeField] private TextMeshProUGUI _descriptionText;
     [SerializeField] private TextMeshProUGUI _quantityText;
     [SerializeField] private TextMeshProUGUI _priceText;
 
@@ -20,7 +19,6 @@ public class StoragePanel : MonoBehaviour
     {
         _nameText.text = itemData.Name;
         _iconImage.sprite = itemData.Icon;
-        _descriptionText.text = itemData.Description;
         _quantityText.text = $"{Random.Range(1, 10)}";
         _priceText.text = $"{Random.Range(1, 10)}";
     }
