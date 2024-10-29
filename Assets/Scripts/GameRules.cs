bf407433c3383e15974821a269491c2230bc8172
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-10-29 22:32:41 +0200

    Changed navigation bar icons

--- Assets/Scripts/GameRules.cs
+++ Assets/Scripts/GameRules.cs
@@ -6,4 +6,5 @@ public class GameRules : ScriptableObject
     [field: SerializeField, Range(1f, 2f)] public float MarketPricesCoef { get; private set; }
     [field: SerializeField, Range(1f, 2f)] public float OrderRewardInCreditsCoef { get; private set; }
     [field: SerializeField, Range(1f, 2f)] public float OrderRewardInExperiencePointsCoef { get; private set; }
+    [field: SerializeField, Range(1f, 2f)] public float NextLevelExperiencePointsCountCoef { get; private set; }
 }
