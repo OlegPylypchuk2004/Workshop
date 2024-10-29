716b765ffffe9c8abb8b590991a2c3273c56a39b
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-10-29 15:13:20 +0200

    Added order data system

---
+++ Assets/Scripts/OrderData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewOrderData", menuName = "Data/Order")]
+public class OrderData : ScriptableObject
+{
+    [field: SerializeField] public string CustomerName { get; private set; }
+    [field: SerializeField] public ItemData[] Items { get; private set; }
+}
