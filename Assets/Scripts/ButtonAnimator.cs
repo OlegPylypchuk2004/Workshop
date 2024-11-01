80f6fed5db0cb350fc63084b05c2c4c5480001fb
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-11-01 15:00:49 +0200

    Added button animator

---
+++ Assets/Scripts/ButtonAnimator.cs
@@ -0,0 +1,69 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Button _button;
+    [SerializeField] private Image _backgroundImage;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void OnValidate()
+    {
+        _button ??= GetComponentInChildren<Button>();
+        _backgroundImage ??= GetComponent<Image>();
+        _text ??= GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_button.interactable)
+        {
+            FadeOut();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        FadeIn();
+    }
+
+    private void FadeIn()
+    {
+        Color targetColor = Color.white;
+
+        if (targetColor == _backgroundImage.color && targetColor == _text.color)
+        {
+            return;
+        }
+
+        _backgroundImage.DOColor(targetColor, 0.1f)
+            .SetEase(Ease.InQuad)
+            .SetLink(gameObject);
+
+        _text.DOColor(targetColor, 0.1f)
+            .SetEase(Ease.InQuad)
+            .SetLink(gameObject);
+    }
+
+    private void FadeOut()
+    {
+        Color targetColor = Color.white * 0.75f;
+        targetColor.a = 1f;
+
+        if (targetColor == _backgroundImage.color && targetColor == _text.color)
+        {
+            return;
+        }
+
+        _backgroundImage.DOColor(targetColor, 0.1f)
+            .SetEase(Ease.OutQuad)
+            .SetLink(gameObject);
+
+        _text.DOColor(targetColor, 0.1f)
+            .SetEase(Ease.OutQuad)
+            .SetLink(gameObject);
+    }
+}
