1f3b2a01288832003f2b67a9a500c1857af44ec8
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-11-04 12:03:34 +0200

    Revert "Changed order panel animation speed"
    This reverts commit 21dd541a3724324cf1c5ee6def8399a668026023.

--- Assets/Scripts/OrderPanel.cs
+++ Assets/Scripts/OrderPanel.cs
@@ -155,17 +155,17 @@ public class OrderPanel : MonoBehaviour
 
         Sequence appearSequence = DOTween.Sequence();
 
-        appearSequence.Append(_rectTransform.DOSizeDelta(normalSize, 0.15f)
+        appearSequence.Append(_rectTransform.DOSizeDelta(normalSize, 0.25f)
             .From(Vector2.zero)
             .SetEase(Ease.OutQuad));
 
-        appearSequence.Append(_backgroundImage.DOFade(1f, 0.15f)
+        appearSequence.Append(_backgroundImage.DOFade(1f, 0.25f)
             .From(0f)
             .SetEase(Ease.OutQuad));
 
-        appearSequence.AppendInterval(0.025f);
+        appearSequence.AppendInterval(0.1f);
 
-        appearSequence.Append(_canvasGroup.DOFade(1f, 0.15f)
+        appearSequence.Append(_canvasGroup.DOFade(1f, 0.25f)
             .From(0f)
             .SetEase(Ease.OutQuad));
 
@@ -183,17 +183,17 @@ public class OrderPanel : MonoBehaviour
 
         Sequence disappearSequence = DOTween.Sequence();
 
-        disappearSequence.Append(_canvasGroup.DOFade(0f, 0.15f)
+        disappearSequence.Append(_canvasGroup.DOFade(0f, 0.25f)
             .From(1f)
             .SetEase(Ease.InQuad));
 
-        disappearSequence.AppendInterval(0.025f);
+        disappearSequence.AppendInterval(0.1f);
 
-        disappearSequence.Append(_backgroundImage.DOFade(0f, 0.15f)
+        disappearSequence.Append(_backgroundImage.DOFade(0f, 0.25f)
             .From(1f)
             .SetEase(Ease.InQuad));
 
-        disappearSequence.Append(_rectTransform.DOSizeDelta(new Vector2(_rectTransform.sizeDelta.x, -50f), 0.15f)
+        disappearSequence.Append(_rectTransform.DOSizeDelta(new Vector2(_rectTransform.sizeDelta.x, -50f), 0.25f)
             .SetEase(Ease.InQuad));
 
         disappearSequence.SetLink(gameObject);
