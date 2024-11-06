7ad379b4287740504bbe6722b42d324d2f5b398a
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-11-06 11:32:45 +0200

    Fixed safe area

--- Assets/Scripts/SafeArea.cs
+++ Assets/Scripts/SafeArea.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 public class SafeArea : MonoBehaviour
 {
+    [System.Flags]
+    public enum SafeAreaSides
+    {
+        None = 0,
+        Top = 1 << 0,
+        Bottom = 1 << 1,
+        Right = 1 << 2,
+        Left = 1 << 3
+    }
+
     [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private SafeAreaSides _sides = SafeAreaSides.None;
 
     private void OnValidate()
     {
@@ -13,13 +24,25 @@ public class SafeArea : MonoBehaviour
     {
         Rect safeArea = Screen.safeArea;
 
-        Vector2 anchorMin = safeArea.position;
-        Vector2 anchorMax = safeArea.position + safeArea.size;
+        Vector2 anchorMin = Vector2.zero;
+        Vector2 anchorMax = Vector2.one;
 
-        anchorMin.x /= Screen.width;
-        anchorMin.y /= Screen.height;
-        anchorMax.x /= Screen.width;
-        anchorMax.y /= Screen.height;
+        if (_sides.HasFlag(SafeAreaSides.Top))
+        {
+            anchorMin.y = safeArea.yMin / Screen.height;
+        }
+        if (_sides.HasFlag(SafeAreaSides.Bottom))
+        {
+            anchorMax.y = safeArea.yMax / Screen.height;
+        }
+        if (_sides.HasFlag(SafeAreaSides.Right))
+        {
+            anchorMin.x = safeArea.xMin / Screen.width;
+        }
+        if (_sides.HasFlag(SafeAreaSides.Left))
+        {
+            anchorMax.x = safeArea.xMax / Screen.width;
+        }
 
         _rectTransform.anchorMin = anchorMin;
         _rectTransform.anchorMax = anchorMax;
