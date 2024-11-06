340e5eff7ad16a6021fb2be9464ad3425594bb6f
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-11-06 13:11:23 +0200

    Added show statistics panel

--- Assets/Scripts/StatisticsPanel.cs
+++ Assets/Scripts/StatisticsPanel.cs
@@ -1,13 +1,35 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatisticsPanel : Panel
 {
     [SerializeField] private TopBar _topBar;
+    [SerializeField] private Image _progressBarFiller;
+    [SerializeField] private TextMeshProUGUI _levelNumberText;
+    [SerializeField] private TextMeshProUGUI _experiencePointsText;
+    [SerializeField] private RectTransform _circleRectTransform;
 
     public override void Open()
     {
         base.Open();
 
         _topBar.SetTitleText("Statistics");
+
+        UpdateView();
+    }
+
+    private void UpdateView()
+    {
+        _levelNumberText.text = $"{LevelManager.GetCurrentLevel()}";
+
+        float currentLevelPoints = LevelManager.GetCurrentLevelPoints();
+        float maxLevelPoints = LevelManager.GetPointsForNextLevel() + LevelManager.GetCurrentLevelPoints();
+
+        _progressBarFiller.fillAmount = currentLevelPoints / maxLevelPoints;
+
+        _circleRectTransform.eulerAngles = new Vector3(0f, 0f, currentLevelPoints / maxLevelPoints * -360f + 180f);
+
+        _experiencePointsText.text = $"{currentLevelPoints} / {maxLevelPoints}";
     }
 }
